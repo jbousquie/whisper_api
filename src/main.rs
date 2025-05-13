e50d813595d02bad3e9ca1f07dd63a0e016ded24
Jérôme Bousquié <jerome.bousquie@iut-rodez.fr>
2025-05-13 09:30:43 +0200

    update cargo + port 8181

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,14 @@ use handlers::transcribe;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("Starting Whisper API server on http://0.0.0.0:8080");
+    println!("Starting Whisper API server on http://0.0.0.0:8181");
     let timeout = std::time::Duration::from_secs(480);
     let keep_alive = std::time::Duration::from_secs(480);
-    
-    HttpServer::new(|| {
-        App::new()
-            .service(transcribe)
-    })
-    .bind("0.0.0.0:8080")?
-    .client_disconnect_timeout(timeout)
-    .keep_alive(keep_alive)
-    
-    .run()
-    .await
+
+    HttpServer::new(|| App::new().service(transcribe))
+        .bind("0.0.0.0:8080")?
+        .client_disconnect_timeout(timeout)
+        .keep_alive(keep_alive)
+        .run()
+        .await
 }
